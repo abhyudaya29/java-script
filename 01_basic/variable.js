3b502dfbb9b93f0b4cd931cc314239f3aaa758be
abhyudaya29 <abhyudayadev123@gmail.com>
2023-07-23 02:53:14 +0530

    variables in java script

---
+++ 01_basic/variable.js
@@ -0,0 +1,12 @@
+const accountID=20202
+let accountEmail="abhyudayadev123@gmail.com"
+var accountPassword="1234"
+accountcity="Delhi"
+
+let accountState;
+console.table([accountEmail,accountID,accountPassword,accountcity,accountState])
+
+/*
+
+Dont use var because of functional and scope issue
+*/
