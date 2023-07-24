b891cb0432b368e127cfa70e3ee10ca491af3bf9
abhyudaya29 <abhyudayadev123@gmail.com>
2023-07-25 00:47:23 +0530

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";// treat all js code as new version of java script
+
+
+console.log("Abhyudaya")
+
+
+let name="abhyudaya"
+let age=null
+let isloggedin=false
+console.table([age,isloggedin])
+
+console.log(typeof(age))
