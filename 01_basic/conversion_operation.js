690b8faa273aebcb5ad4f9f6093ac5d5900df851
abhyudaya29 <abhyudayadev123@gmail.com>
2023-07-25 20:24:50 +0530

    strings and conversion

---
+++ 01_basic/conversion_operation.js
@@ -0,0 +1,9 @@
+let score="33"
+
+console.log(typeof score)
+
+let Name="abhyudaya"
+console.log(typeof (Name))
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber)
---
+++ 01_basic/string.js
@@ -0,0 +1,31 @@
+const name="abhyudaya"
+const repocount=50
+
+console.log(`hello my name is ${name } and my repo count is ${repocount}`)
+// String intropolation
+
+const game_name=new String("Abhyudayaaa")
+console.log(typeof game_name)
+
+console.log(game_name.__proto__)  // we get an object
+console.log(game_name.length)
+console.log(game_name.indexOf("d"))
+console.log(game_name.toUpperCase())
+console.log(game_name.charAt(2))
+console.log(game_name.split(2))
+const newstring =game_name.substring(0,5)
+// split the srting in 2 halfs
+// console.log(newstring)
+
+const another_string=game_name.slice(-8,4)
+console.log(another_string)
+
+const newstringone=" dev "
+console.log(newstringone)
+console.log(newstringone.trim())
+
+
+const url="http://abhyudaya.com/abhyudaya%20dubey"
+console.log(url.replace("%20","-"))
+
+// replaces the value
