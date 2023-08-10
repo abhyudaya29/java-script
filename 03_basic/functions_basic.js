e1b391987d15a1a3e2babb93a9f5efd7defaec41
abhyudaya29 <abhyudayadev123@gmail.com>
2023-08-10 18:51:55 +0530

    factory and constructor functions

--- 03_basic/functions_basic.js
+++ 03_basic/functions_basic.js
@@ -15,3 +15,41 @@ function CreateRectange(len,wid){
 
 let rectangleobj1=CreateRectange(2,2);
 console.log(rectangleobj1.area)
+
+// Constructor function
+
+function Rectangle(){
+    this.length=1;
+    this.width=2;
+    this.draw=function(){
+        console.log('drawing');
+    }
+}
+
+let Rec=new Rectangle();
+Rec.color="Red";
+delete Rec.color;
+console.log(Rec)
+
+console.log(Rec.constructor)
+
+console.log(Rectangle.constructor)
+
+
+let rc1=new Rectangle()
+
+
+let Rectangle=new Function(`
+    this.length=1;
+    this.width=2;
+    this.draw=function(){
+        console.log('drawing');
+    }
+`)
+
+console.log(Rectangle);
+console.log(Rectangle.constructor)
+
+let obj1=new Rectangle
+console.log(obj1)
+console.log(obj1.constructor)
