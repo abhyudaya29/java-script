530a9ab5e1795b8ba54cc1e6ed44d3e3d2a39bb5
abhyudaya29 <abhyudayadev123@gmail.com>
2023-08-16 01:20:59 +0530

    dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,1 +1,6 @@
 console.log("jai hind")
+
+
+let song=document.getElementsByClassName('songBanner')
+// let song =document.getElementsByClassName('songBanner')
+let pro=document.getElementById('progressbar')
