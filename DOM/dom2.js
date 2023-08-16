3c00e63df11d0bafa5714407b24c1ba413a14800
abhyudaya29 <abhyudayadev123@gmail.com>
2023-08-17 01:25:48 +0530

    event listner1

--- DOM/dom2.js
+++ DOM/dom2.js
@@ -3,3 +3,14 @@
 //            2)remove event listner()
 //            3) dispatch event()
 // listner means what action need to be taken after the specific event
+// 
+
+
+
+
+// add event listner
+// document.addEventListener('click',function(){console.log('hey baby')})
+let a=document.querySelector('h1')
+a.addEventListener('click',function(){
+    a.style.background='green'
+})
