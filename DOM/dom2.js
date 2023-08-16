e80c945cf4841758badd906c529af9dc2852577a
abhyudaya29 <abhyudayadev123@gmail.com>
2023-08-16 23:40:38 +0530

    event listner

---
+++ DOM/dom2.js
@@ -0,0 +1,5 @@
+// event target-->interface -->to level entity
+// 3 methods 1) add event listner()
+//            2)remove event listner()
+//            3) dispatch event()
+// listner means what action need to be taken after the specific event
