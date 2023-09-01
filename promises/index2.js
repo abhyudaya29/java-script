7df1cf26270a05ca4a003553287a7bd81f503644
abhyudaya29 <abhyudayadev123@gmail.com>
2023-09-02 03:02:29 +0530

    APIS

--- promises/index2.js
+++ promises/index2.js
@@ -80,3 +80,4 @@ async function consume5(){
 }
 
 consume5()
+
---
+++ promises/index3.js
@@ -0,0 +1,97 @@
+// let merapromiese=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("Babe i'm inside promise")
+//     },5000)
+    
+//     // resolve(1998)
+//     reject(new Error("Baby error aya hai"))
+    
+// })
+
+// merapromiese.then((value)=>{
+//     console.log(value)
+// })
+// merapromiese.catch((error)=>{
+//     console.log("received an error")
+// })
+// // merapromiese
+
+
+// // let merapromiese2=new Promise(function(resolve,reject){
+// //     setTimeout(function(){
+// //         console.log("Babe i'm inside promise2")
+// //     },3000)
+// //     // resolve(1998)
+// //     // reject(new Error("Baby error aya hai2"))
+    
+// // })
+
+// console.log("Phela ")
+
+
+
+// let wadda1=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("set time out1 started")
+//     },2000)
+//     resolve()
+// })
+
+// wadda1.then(function(){
+//     let wadda2=new Promise(function(resolve,reject){
+//         resolve("Wadda2 resolved")
+//     })
+//     return wadda2
+// }).then((function(value){
+//     console.log(value)
+// }))
+
+
+
+// async function utility(){
+//     let mh=new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             resolve("mh is hot")
+//         },5000)
+//     })
+    
+//     let dl=new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             resolve("cool")
+//         },6000)
+//     })
+//     let mumbai=await mh
+//     let delhi=await dl
+//     return[mh,dl]
+// }
+
+// async function utility(){
+//     let content=await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     let output= await content.json()
+//     console.log(output)
+// }
+// utility()
+
+
+async function helper(){
+    let option={
+        method: "POST",
+    body: JSON.stringify({
+        userId: 1,
+        title: "Taggadi",
+        completed: false
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }
+    let content=await fetch("https://jsonplaceholder.typicode.com/todos",option)
+    let response=content.json()
+    return response
+
+}
+async function utility(){
+    let ans=await helper()
+    console.log(ans)
+}
+utility()
