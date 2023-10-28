75d638013679dbab5c6f760d4a5ccabf01b5f10d
abhyudaya29 <abhyudayadev123@gmail.com>
2023-10-28 13:06:57 +0530

    commited controol flow

---
+++ controol_flow/index.js
@@ -0,0 +1,19 @@
+// const user="abhyudaya"
+// if(user){
+//     console.log("user exist");
+// }
+// else{
+//     console.log("Not exist");
+// }
+
+
+const score=300;
+if(score>=200){
+    var power=400;
+    console.log(`sore is more than ${score}`);
+    // console.log(power)
+}
+else{
+    console.log(`score is less than ${score}`);
+}
+console.log(power);
