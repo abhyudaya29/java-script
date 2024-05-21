5e047f0a12d9666abea2d54fcf24e7a3f233fc19
abhyudaya29 <abhyudayadev123@gmail.com>
2024-05-21 09:24:04 +0530

    scope in js

---
+++ akshaySaini/scopeChaning.js
@@ -0,0 +1,31 @@
+// function a(){
+//     console.log(b)
+// }
+
+// var b=10
+// a()
+// function c(){
+//     console.log(d)
+// }
+// let d=20
+// c()
+
+// function print(){
+//     var abhyudaya="dubey"
+//     console.log(abhyudaya)
+// }
+// console.log(abhyudaya)
+
+
+function a(){
+    var name="Abhyudaya"
+    function c(){
+        console.log(name,">>>name")
+        
+    }
+    c()
+    console.log("A is executed")
+}
+
+a()
+console.log(name<">>name outside the scope")
